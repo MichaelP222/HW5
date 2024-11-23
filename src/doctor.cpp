5f979912e63a68d2a517a59ef8a78b659ef9a6d0
Yinuo Peng <yinuop2@uci.edu>
2024-11-23 01:43:55 +0000

    implement doctor class

--- src/doctor.cpp
+++ src/doctor.cpp
@@ -1,1 +1,19 @@
 // Implementation file doctor.cpp
+#include "../include/doctor.h"
+#include <iostream>
+
+Doctor::Doctor(const std::string& first, const std::string& last, const std::string& spl)
+    : Person(first, last), specialty(spl) {}
+
+void Doctor::setSpecialty(const std::string& spl) {
+    specialty = spl;
+}
+
+std::string Doctor::getSpecialty() const {
+    return specialty;
+}
+
+void Doctor::print(std::ostream& out) const {
+    Person::print(out);
+    out << " " << specialty;
+}
