1d030e9af5b72d3ebf5658ec8f81c14b33b0043d
Yinuo Peng <yinuop2@uci.edu>
2024-11-23 01:21:24 +0000

    implement person class

--- src/person.cpp
+++ src/person.cpp
@@ -1,1 +1,19 @@
 // Implementation file person.cpp
+#include "../include/person.h"
+#include <iostream>
+#include <string>
+
+Person::Person(const std::string & first, const std::string & last)
+    : firstName(first), lastName(last) {}
+
+string Person::getFirstName() const {
+    return firstName;
+}
+
+string Person::getLastName() const {
+    return lastName;
+}
+
+void print(std::ostream &os) const {
+    os << firstName << " " << lastName;
+}
