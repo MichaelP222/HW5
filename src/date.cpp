cde49b68800978691991efa979800402548a6c15
Yinuo Peng <yinuop2@uci.edu>
2024-11-23 02:14:30 +0000

    implement patient class

--- src/date.cpp
+++ src/date.cpp
@@ -1,1 +1,25 @@
 // Implementation file date.cpp
+#include "../include/date.h"
+
+int Date::getDay() const {
+    return dDay;
+}
+
+int Date::getMonth() const {
+    return dMonth;
+}
+
+int Date::getYear() const {
+    return dYear;
+}
+
+void Date::printDate(std::ostream& out) const {
+    out << dMonth << "-" << dDay << "-" << dYear;
+}
+
+Date::Date(int month, int day, int year)
+    : dMonth(month), dDay(day), dYear(year) {}
+
+bool operator==(const Date& other) const noexcept {
+    return (dMonth == other.dMonth && dDay == other.dDay && dYear == other.dYear);
+}
